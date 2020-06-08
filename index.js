44dcf9e1640e750ff3b1d96e28c392ed8c221dba
Bradley Beggs <brad.beggs@gmail.com>
2020-06-08 03:14:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Howdy";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Welcome" + specialGuest + "!";
 
-var topic = "space";
+var topic = "Let\'s talk about space.";
 
 var conversation = `${topic}`;
